3e008e073b378a9e55cbff29a686931b4025043d
rsilveirafh <ricardodasilveira@gmail.com>
2022-10-14 13:49:27 -0300

    adding newpost bash scr

---
+++ posts/newpost.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# Criando a pasta
+echo "Type the post's name (00X-name):"
+read name
+
+mkdir $name
+sleep 1
+echo "New $name folder"
+sleep 1
+echo "Creating new index.qmd file"
+cat > $name/index.qmd <<EOF
+---
+title: "A great title"
+subtitle: "A great subtitle"
+author: "Ricardo"
+date: "2022-XX-XX"
+categories: [R, tags]
+image: "*.png"
+draft: true
+---
+
+\`\`\`{r}
+#|label: setup
+#|include: false
+knitr::opts_chunk$set(echo=TRUE, warning=FALSE, message=FALSE)
+\`\`\`
+
+## What this post is about?
+
+
+
+## History
+
+
+
+## Finishing
+
+
+EOF
+
+echo "$name/index.qmd created"
